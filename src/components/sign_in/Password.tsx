6f276c32acc25499e6ca899c4e4f4b279370ffdd
ryan <ryan.z.williams@gmail.com>
2023-06-24 00:26:04 +0800

    fix

--- src/components/sign_in/Password.tsx
+++ src/components/sign_in/Password.tsx
@@ -1,17 +1,16 @@
 "use client";
+
 export function Password() {
   return (
     <>
       <div className="flex gap-3 justify-end items-center">
         <label htmlFor="password">密码:</label>
-        <div>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            className="h-10 p-2.5"
-          />
-        </div>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          className="h-10 p-2.5"
+        />
       </div>
     </>
   );
