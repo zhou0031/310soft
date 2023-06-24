8d4d9bc216116dc68d02e18f6abd4a6431ed6bce
ryan <ryan.z.williams@gmail.com>
2023-06-24 16:30:34 +0800

    signin css

--- src/app/sign_in/page.tsx
+++ src/app/sign_in/page.tsx
@@ -3,14 +3,16 @@ import { Email } from "@/components/sign_in/Email";
 import { Password } from "@/components/sign_in/Password";
 import { Google } from "@/components/sign_in/Google";
 import { Facebook } from "@/components/sign_in/Facedbook";
+import { GoPerson } from "react-icons/go";
 
 export default function Index() {
   return (
     <>
-      <div className="flex flex-col items-center my-56 ">
+      <div className="w-[400px] flex flex-col items-center my-36 mx-auto py-5 bg-gray-200">
         <header>
           <h1 className="text-2xl mb-2">登陆</h1>
         </header>
+        <GoPerson size={100} />
         <div className="flex flex-col gap-2">
           <form className="flex flex-col gap-3">
             <Email />
@@ -24,10 +26,26 @@ export default function Index() {
               </button>
             </div>
           </form>
+
+          <Link
+            href="#"
+            className="flex justify-end font-sans font-extralight text-slate-700"
+          >
+            忘记密码?
+          </Link>
+
           <hr className="h-px border-0 dark:bg-gray-400"></hr>
+
           <Google />
           <Facebook />
         </div>
+
+        <Link
+          href="/sign_up"
+          className="mt-10 font-sans font-extralight text-slate-700"
+        >
+          创建账户
+        </Link>
       </div>
     </>
   );
