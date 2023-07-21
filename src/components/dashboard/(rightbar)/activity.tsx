c6d2d0e31b21c7e671f6ae1b7a1b227c25c56378
ryan <ryan.z.williams@gmail.com>
2023-07-21 12:36:07 +0800

    activity line

--- src/components/dashboard/(rightbar)/activity.tsx
+++ src/components/dashboard/(rightbar)/activity.tsx
@@ -1,7 +1,13 @@
 export default function Activity() {
   return (
     <div className="flex flex-col w-full gap-2">
-      <h3 className="text-center text-sm font-serif font-medium">近期事件</h3>
+      <h3 className="w-full relative inline-flex items-center justify-center text-sm font-serif font-medium">
+        <hr className="w-full  my-4" />
+        <span className="absolute px-3 font-medium -translate-x-1/2 bg-white left-1/2">
+          近期事件
+        </span>
+      </h3>
+
       <div className="text-sm">
         Russia is introducing a notification procedure for movement through the
         territory of Russia for employees of British diplomatic missions, the
