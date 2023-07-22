4ff7651da0d2a98564c08273ed84b0f082ead48f
ryan <ryan.z.williams@gmail.com>
2023-07-22 21:24:11 +0800

    loading...

--- src/app/(user)/dashboard/loading.tsx
+++ src/app/(user)/dashboard/loading.tsx
@@ -1,3 +1,39 @@
 export default function Loading() {
-  return <>Loading ................</>;
+  return (
+    <>
+      <span>数据加载...</span>
+      <div role="status" className="space-y-2.5 animate-pulse w-full">
+        <div className="flex items-center w-full space-x-2">
+          <div className="h-2.5 bg-gray-200 rounded-full dark:bg-slate-300 w-32"></div>
+          <div className="h-2.5 bg-gray-300 rounded-full dark:bg-slate-400 w-24"></div>
+          <div className="h-2.5 bg-gray-300 rounded-full dark:bg-slate-400 w-full"></div>
+        </div>
+        <div className="flex items-center w-full space-x-2">
+          <div className="h-2.5 bg-gray-200 rounded-full dark:bg-slate-300 w-full"></div>
+          <div className="h-2.5 bg-gray-300 rounded-full dark:bg-slate-400 w-full"></div>
+          <div className="h-2.5 bg-gray-300 rounded-full dark:bg-slate-400 w-24"></div>
+        </div>
+        <div className="flex items-center w-full space-x-2">
+          <div className="h-2.5 bg-gray-300 rounded-full dark:bg-slate-300 w-full"></div>
+          <div className="h-2.5 bg-gray-200 rounded-full dark:bg-slate-400 w-80"></div>
+          <div className="h-2.5 bg-gray-300 rounded-full dark:bg-grslateay-400 w-full"></div>
+        </div>
+        <div className="flex items-center w-full space-x-2">
+          <div className="h-2.5 bg-gray-200 rounded-full dark:bg-slate-300 w-full"></div>
+          <div className="h-2.5 bg-gray-300 rounded-full dark:bg-slate-400 w-full"></div>
+          <div className="h-2.5 bg-gray-300 rounded-full dark:bg-slate-500 w-24"></div>
+        </div>
+        <div className="flex items-center w-full space-x-2">
+          <div className="h-2.5 bg-gray-300 rounded-full dark:bg-slate-300 w-32"></div>
+          <div className="h-2.5 bg-gray-300 rounded-full dark:bg-slate-400 w-24"></div>
+          <div className="h-2.5 bg-gray-200 rounded-full dark:bg-slate-100 w-full"></div>
+        </div>
+        <div className="flex items-center w-full space-x-2">
+          <div className="h-2.5 bg-gray-300 rounded-full dark:bg-slate-100 w-full"></div>
+          <div className="h-2.5 bg-gray-200 rounded-full dark:bg-slate-200 w-80"></div>
+          <div className="h-2.5 bg-gray-300 rounded-full dark:bg-slate-300 w-full"></div>
+        </div>
+      </div>
+    </>
+  );
 }
