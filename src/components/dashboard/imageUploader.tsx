dc5349b5947ab13134712bd047f4109f2cf22bb4
ryan <ryan.z.williams@gmail.com>
2023-07-30 13:57:44 +0800

    upload icon

--- src/components/dashboard/imageUploader.tsx
+++ src/components/dashboard/imageUploader.tsx
@@ -1,8 +1,9 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDropzone } from "react-dropzone";
 import Image from "next/image";
 import { PiUploadLight } from "react-icons/pi";
+import { IoMdCheckmarkCircleOutline } from "react-icons/io";
 
 export default function ImageUploader() {
   const MAX_SIZE = 2 * 1024 * 1024;
@@ -30,19 +31,17 @@ export default function ImageUploader() {
   return (
     <>
       <div className="flex flex-col gap-5">
-        <div
-          {...getRootProps()}
-          className={`dropzone ${isDragActive ? "active" : ""}`}
-        >
+        <div {...getRootProps()}>
           <input {...getInputProps()} />
-
           <div className="flex justify-center items-center gap-5 p-10 bg-slate-200 text-slate-400 rounded-lg hover:text-black cursor-pointer">
-            <PiUploadLight size={50} />
-            <div className="flex flex-col">
-              <p className="font-serif text-sm">
-                上传头像 (JPEG, PNG, 不超过2MB)
-              </p>
-              <p className="font-serif text-sm">点击上传或图片拖放至此</p>
+            {isDragActive ? (
+              <IoMdCheckmarkCircleOutline size={50} />
+            ) : (
+              <PiUploadLight size={50} />
+            )}
+            <div className="flex flex-col break-all font-serif text-sm">
+              <p>上传头像 (JPEG, PNG, 不超过2MB)</p>{" "}
+              <p>点击上传或图片拖放至此</p>
             </div>
           </div>
         </div>
