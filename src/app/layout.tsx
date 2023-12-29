82ced772882b096ae5e1139dd49b3fd98b8d1b9b
ryan <ryan.z.williams@gmail.com>
2023-12-29 15:07:18 +0800

    min-h-screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} h-screen w-screen flex justify-center items-center bg-gray-300`}
+        className={`${inter.className} flex justify-center items-center  bg-gray-300 min-h-screen`}
       >
         <ProviderWrapper>{children}</ProviderWrapper>
       </body>
