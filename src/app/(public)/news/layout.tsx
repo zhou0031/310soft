8e0f46c85b87086a1fe8a11155fa910f3aa9e6ed
ryan <ryan.z.williams@gmail.com>
2023-12-31 21:53:50 +0800

    layout md

--- src/app/(public)/news/layout.tsx
+++ src/app/(public)/news/layout.tsx
@@ -1,7 +1,7 @@
 export default function NewsLayout({ children }) {
   return (
     <>
-      <div className="bg-white flex flex-wrap w-5/6 gap-2 justify-center h-fit max-lg:w-full p-5">
+      <div className="bg-white flex flex-wrap justify-between min-w-[100rem] max-w-[100rem] gap-2 h-fit p-5 max-md:h-full">
         {children}
       </div>
     </>
