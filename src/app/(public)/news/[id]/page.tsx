f80a61a4ec809a60b98e82351c0e99001f052045
ryan <ryan.z.williams@gmail.com>
2024-01-06 03:25:56 +0800

    news id

--- src/app/(public)/news/[id]/page.tsx
+++ src/app/(public)/news/[id]/page.tsx
@@ -1,33 +1,40 @@
-/*
-export async function generateStaticParams() {
-  const response = await fetch("http://localhost:3000/api/news");
+import { NextResponse } from "next/server";
+import { prisma } from "../../../../prismaDB";
 
-  const json = await response.json();
-  const news = json?.body?.data;
-
-  return news.map((item) => ({
-    id: item.id,
-  }));
-}
-/*
 async function getNewsById(id) {
+  "use server";
   try {
-    const response = await fetch(`http://localhost:3000/api/news/${id}`);
-    return await response.json();
+    const response = await prisma.news.findUnique({
+      where: { id: id },
+    });
+    return NextResponse.json({
+      status: 200,
+      body: {
+        success: true,
+        data: response,
+      },
+    });
   } catch (e) {
-    console.log(e);
+    return NextResponse.json({
+      status: 500,
+      body: {
+        success: false,
+        error: "Internal Server Error",
+      },
+    });
+  } finally {
+    await prisma.$disconnect();
   }
 }
-*/
+
 export default async function Page({ params }) {
   const { id } = params;
-  /*
   const data = await getNewsById(id);
+  const json = await data.json();
+  const news = json?.body?.data;
   return (
     <>
-      <div className="">{data.content}</div>
+      <div className="">{JSON.stringify(news)}</div>
     </>
   );
-  */
-  return <></>;
 }
