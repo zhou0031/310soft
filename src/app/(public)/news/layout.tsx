43a27cc0e03183a77d245ac8562f88c559e17614
ryan <ryan.z.williams@gmail.com>
2024-01-18 12:31:32 +0800

    layout

--- src/app/(public)/news/layout.tsx
+++ src/app/(public)/news/layout.tsx
@@ -1,7 +1,7 @@
 export default function NewsLayout({ children }) {
   return (
     <>
-      <div className="bg-white mx-40">{children}</div>
+      <div className="bg-white mx-40 max-lg:mx-0">{children}</div>
     </>
   );
 }
