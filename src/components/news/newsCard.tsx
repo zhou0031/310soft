677bee68da7aca74e1ea6faec7b519acc2c97f39
ryan <ryan.z.williams@gmail.com>
2024-02-03 01:17:51 +0800

    news card flex-grow

--- src/components/news/newsCard.tsx
+++ src/components/news/newsCard.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import styles from "../../../styles/scrollbar.module.css";
+import Reads from "./reads";
 
 export default function NewsCard(news) {
   let obj;
@@ -16,7 +17,7 @@ export default function NewsCard(news) {
   return (
     <>
       <div
-        className={`w-52 h-96 bg-slate-100 border border-gray-200 rounded-lg shadow overflow-auto ${styles["my-custom-scrollbar"]}`}
+        className={`flex flex-col w-52 h-96 bg-slate-100 border border-gray-200 rounded-lg shadow overflow-auto ${styles["my-custom-scrollbar"]}`}
       >
         <Link href={`/news/${news.id}`}>
           {obj?.src && (
@@ -29,26 +30,27 @@ export default function NewsCard(news) {
             />
           )}
         </Link>
-        <div className="p-2">
+        <div className="p-2 flex-grow flex flex-col">
           <Link href={`/news/${news.id}`}>
             <h5 className="mb-2 text-lg font-bold tracking-tight text-gray-900">
               {news.title}
             </h5>
           </Link>
           <p className="mb-3 text-sm text-gray-400">
-            {news.content.toString().slice(0, 60)}
+            {news.content.toString().slice(0, 80)}
           </p>
-          <time
-            className="text-xs text-gray-500"
-            dateTime={`${news.published_at}`}
-          >
-            {new Date(news.published_at).toLocaleString("zh-CN", {
-              year: "numeric",
-              month: "long",
-              day: "numeric",
-              timeZone: "UTC",
-            })}
-          </time>
+          <div className="flex-grow"></div>
+          <div className="flex justify-between text-xs text-gray-500">
+            <time dateTime={`${news.published_at}`}>
+              {new Date(news.published_at).toLocaleString("zh-CN", {
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+                timeZone: "UTC",
+              })}
+            </time>
+            <Reads number={1} />
+          </div>
         </div>
       </div>
     </>
---
+++ src/components/news/reads.tsx
@@ -0,0 +1,7 @@
+export default function Reads({ number }) {
+  return (
+    <>
+      <p>已读{number}次</p>
+    </>
+  );
+}
