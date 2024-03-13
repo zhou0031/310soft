98dd3f413b510fe18a39939cf9cf02ed76367bcb
ryan williams <ryan.z.williams@gmail.com>
2024-03-13 14:43:30 +0800

    react query

--- src/app/ProviderWrapper.tsx
+++ src/app/ProviderWrapper.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { SessionProvider } from "next-auth/react";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
--- src/components/news/comments.tsx
+++ src/components/news/comments.tsx
@@ -1,7 +1,9 @@
 "use client";
+import styles from "../../../styles/scrollbar.module.css";
 import { useSession } from "next-auth/react";
 import { DefaultSession } from "next-auth";
 import { useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
 import io from "socket.io-client";
 
 declare module "next-auth" {
@@ -14,8 +16,17 @@ declare module "next-auth" {
 
 export default function Comments({ newsId }) {
   const { data: session, status } = useSession();
+  const { isPending, error, data } = useQuery({
+    queryKey: ["comments"],
+    queryFn: () =>
+      fetch("https://jsonplaceholder.typicode.com/comments").then((res) =>
+        res.json()
+      ),
+  });
 
+  /*
   useEffect(() => {
+    
     const socket = io("http://localhost:3001/news");
 
     socket.on("comments", (data) => {
@@ -25,12 +36,24 @@ export default function Comments({ newsId }) {
     return () => {
       socket.disconnect();
     };
-  }, [newsId]);
+    
 
+  }, [newsId]);
+  */
+  if (isPending) return "读取中 ...";
+  if (error) return `${error.message} 读取失败`;
   return (
     <>
-      <div className="text-lg font-bold text-gray-500">1983 条评论</div>
-      <div className="w-full h-3/5 min-h-[620px] max-h-[860px]"></div>
+      <div className="text-lg font-bold text-gray-500">
+        {data.length} 条评论
+      </div>
+      <div
+        className={`w-full h-3/5 min-h-[620px] max-h-[860px] px-5 overflow-auto ${styles["my-custom-scrollbar"]}`}
+      >
+        {data.map((comment) => (
+          <p key={comment.id}>{comment.name}</p>
+        ))}
+      </div>
     </>
   );
 }
